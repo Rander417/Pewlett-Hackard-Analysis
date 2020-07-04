5f254c6fdc4aea9efa921d281ccb380a7db019ee
Richard Anderson <rich417@gmail.com>
2020-07-04 15:54:35 -0400

    Completed deliverable 2. Preparing for submission

--- schema.sql
+++ schema.sql
@@ -61,15 +61,18 @@ SELECT * FROM employees;
 SELECT * FROM salaries;
 SELECT * FROM titles;
 
--- END SCHEMA ----------------------------------------------------------
+-- END SCHEMA --------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 
 
--- QUERIES -------------------------------------------------------------
 
--- Deliverable#1 
--- Section#1- Create a new table of eligible retirees currently working
+----------------------------------------------------------------------------------------------------------------------------------
+-- QUERIES -----------------------------------------------------------------------------------------------------------------------
 
--- Filter the "employees" to only those eligible to retire and exported to a new table
+-- Deliverable#1 --------------------------------------------------------------------------------------------------------------------------
+-- 	Section#1- Create a new table of eligible retirees currently working
+
+-- 	Filter the "employees" to only those eligible to retire and exported to a new table
 SELECT employees.first_name, employees.last_name
 INTO retirement_info
 FROM employees
@@ -77,28 +80,16 @@ LEFT JOIN titles
 ON employees.emp_no = titles.emp_no
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
--- Check the table
+-- 	Check the table
 SELECT * FROM retirement_info;
 
---Create table of employees eligible to retire AND are still employed
-SELECT retirement_info.emp_no,
-	retirement_info.first_name,
-	retirement_info.last_name,
-	dept_emp.from_date
-INTO current_eligible_emp
-FROM retirement_info
-LEFT JOIN dept_emp
-ON retirement_info.emp_no = dept_emp.emp_no 
-WHERE dept_emp.to_date = ('9999-01-01');
-
-
--- Creates a new table containing only the current employees who are eligible for retirement grouped by title
--- Using (3) joins to add the title and salaries
+-- 	Creates a new table containing only the current employees who are eligible for retirement grouped by title
+-- 	Using (3) joins to add the department info, titles and salaries
 SELECT retirement_info.emp_no,
 	retirement_info.first_name,
 	retirement_info.last_name,
 	titles.title,
-	dept_emp.from_date,
+	titles.from_date,
 	salaries.salary
 	
 INTO current_eligible_emp
@@ -113,37 +104,73 @@ ON titles.emp_no = salaries.emp_no
 
 WHERE dept_emp.to_date = ('9999-01-01')
 
-ORDER BY titles.title;
+ORDER BY retirement_info.emp_no;
 
-
-DROP TABLE IF EXISTS current_eligible_emp;
-SELECT * FROM current_eligible_emp;
+--DROP TABLE IF EXISTS current_eligible_emp;
 -- -------------------------------------------------------------
+
+
 -- Deliverable#1 
--- Section#2- Number of Retiring Employees by Title
+-- 	Section#2- Number of Retiring Employees by Title
+-- 	There are duplicates because some employees have switched titles over the years. THese need to be addressed 
+SELECT * FROM current_eligible_emp;
 
 
--- Employee count by department number (note the results are in random order)
-SELECT COUNT(ce.emp_no), de.dept_no
-FROM current_emp as ce
-LEFT JOIN dept_emp as de
-ON ce.emp_no = de.emp_no
-GROUP BY de.dept_no;
+-- Deliverable#1 
+-- 	Section#3- A new table that includes only the most recent title of each employee. 
+-- 	Duplicates will be removed
+SELECT	*
+INTO eligible_emp_noDups
+From (SELECT current_eligible_emp.emp_no,
+		current_eligible_emp.first_name,
+		current_eligible_emp.last_name,
+		current_eligible_emp.title,
+		current_eligible_emp.from_date,
+		current_eligible_emp.salary, ROW_NUMBER() OVER
+ 		(PARTITION BY (current_eligible_emp.emp_no)
+ 		ORDER BY current_eligible_emp.from_date DESC) rn
+ 		FROM current_eligible_emp
+	 	) tmp WHERE rn = 1
 
--- Employee count by department number (ordered)
-SELECT COUNT(ce.emp_no), de.dept_no
-FROM current_emp as ce
-LEFT JOIN dept_emp as de
-ON ce.emp_no = de.emp_no
-GROUP BY de.dept_no
-ORDER BY de.dept_no;
+ALTER TABLE eligible_emp_noDups
+DROP COLUMN rn;
 
---skills challenge
-SELECT COUNT(ce.emp_no), de.dept_no
-INTO groupby_dept
-FROM current_emp as ce
-LEFT JOIN dept_emp as de
-ON ce.emp_no = de.emp_no
-GROUP BY de.dept_no
-ORDER BY de.dept_no;
+SELECT * FROM eligible_emp_noDups;
 
+-- END DELIVERABLE #1 --------------------------------------------------------------------------------------------------------------------------  
+
+
+-- Deliverable 2 -------------------------------------------------------------------------------------------------------------------------------
+-- Mentorship Eligibility 
+-- 	Needs: Employee number, First name, last name, Title, from_date, to_date
+-- 	Birthday falls between January 1, 1965 and December 31, 1965
+
+-- Query creates the base data but has the duplicates due to multiple titles.
+SELECT employees.emp_no, employees.first_name, employees.last_name, titles.title, titles.from_date, titles.to_date
+INTO mentorship_eligible_raw
+FROM employees
+LEFT JOIN titles
+ON employees.emp_no = titles.emp_no
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31');
+-- 	Check the table
+SELECT * FROM mentorship_eligible_raw;
+--DROP TABLE IF EXISTS mentorship_eligible;
+
+-- Query to clear duplicates
+SELECT	*
+INTO mentorship_eligible
+From (SELECT mentorship_eligible_raw.emp_no,
+		mentorship_eligible_raw.first_name,
+		mentorship_eligible_raw.last_name,
+		mentorship_eligible_raw.title,
+		mentorship_eligible_raw.from_date,
+		mentorship_eligible_raw.to_date, ROW_NUMBER() OVER
+ 		(PARTITION BY (mentorship_eligible_raw.emp_no)
+ 		ORDER BY mentorship_eligible_raw.from_date DESC) rn
+ 		FROM mentorship_eligible_raw
+	 	) tmp WHERE rn = 1
+
+ALTER TABLE mentorship_eligible
+DROP COLUMN rn;
+
+SELECT * FROM mentorship_eligible;
